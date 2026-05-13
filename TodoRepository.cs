ec1173fd1fa8854657420fc251230b801c65f4fd
duytienkaka <duytienkaka123az@gmail.com>
2026-05-13 16:18:20 +0700

    Update todo repository

--- TodoRepository.cs
+++ TodoRepository.cs
@@ -1,10 +1,12 @@
+using System.Runtime.CompilerServices;
+
 namespace TodoApp
 {
     public class TodoRepository
     {
         private readonly List<Todo> _todos;
         private readonly string _path = "todos.txt";
-        private readonly int _nextId = 1;
+        private int _nextId = 1;
         public TodoRepository()
         {
             _todos = [];
@@ -16,8 +18,56 @@ namespace TodoApp
             if (!File.Exists(_path)) return;
             foreach( var line in File.ReadAllLines(_path))
             {
-                _todos.Add(Todo.FromFileString(line));
+                var item = Todo.FromFileString(line);
+                _todos.Add(item);
+                if(item.Id >= _nextId)
+                {
+                    _nextId = item.Id + 1;
+                }
             }
+        }
+        private void SaveToFile()
+        {
+            var lines = _todos.Select(t => t.ToFileString()).ToArray();
+            File.WriteAllLines(_path, lines);
+        }
+        public List<Todo> GetAll()
+        {
+            return _todos;
+        }
+        public Todo AddTodo(string title)
+        {
+            var item = new Todo
+            {
+                Id = _nextId++,
+                Title = title,
+                IsCompleted = false
+            };
+            _todos.Add(item);
+            SaveToFile();
+            return item;
+        }
+        public bool RemoveTodo(int id)
+        {
+            var item = _todos.FirstOrDefault(t => t.Id == id);
+            if (item != null)
+            {
+                _todos.Remove(item);
+                SaveToFile();
+                return true;
+            }
+            return false;
+        }
+        public bool ToggleCompleted(int id)
+        {
+            var item = _todos.FirstOrDefault(t => t.Id == id);
+            if (item != null)
+            {
+                item.IsCompleted = !item.IsCompleted;
+                SaveToFile();
+                return true;
+            }
+            return false;
         }
     }
 }
